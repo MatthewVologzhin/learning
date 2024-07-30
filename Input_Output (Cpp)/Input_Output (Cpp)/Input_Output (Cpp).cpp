68b1ec61457ad86bdd312d173cc79b58fd1b00dc
Матвей Вологжин <matvey.vologzhin@mail.ru>
2024-07-30 22:45:48 +0800

    First steps in C++. I try to write the code which asks username and welcomes he.

---
+++ Input_Output (Cpp)/Input_Output (Cpp)/Input_Output (Cpp).cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+string username;
+
+int main() {
+	setlocale(LC_ALL, "RU");
+
+	cout << "Введите имя пользователя: ";
+	cin >> username;
+	cout << "Здравствуйте, " << username << "!";
+	return 0;
+}
